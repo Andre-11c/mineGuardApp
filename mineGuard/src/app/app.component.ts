3f6a4749cfae93d63c6e28df26924f207b65661c
Andre Chipana <andrechipanarios.11@gmail.com>
2024-11-14 23:28:34 -0500

    Creacion de nuevos componentes como caja de comentarios header para el asunto del post.

--- mineGuard/src/app/app.component.ts
+++ mineGuard/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NavBarComponent} from './shared/components/nav-bar/nav-bar.component';
+import {PostComponent} from './post/post.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent],
+  imports: [RouterOutlet, NavBarComponent, PostComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- mineGuard/src/app/post/post.component.ts
+++ mineGuard/src/app/post/post.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {EncabezadoPostComponent} from './encabezado-post/encabezado-post.component';
+import {ComentariosPublicadosComponent} from './comentarios-publicados/comentarios-publicados.component';
 
 @Component({
   selector: 'app-post',
   standalone: true,
-  imports: [],
+  imports: [
+    EncabezadoPostComponent,
+    ComentariosPublicadosComponent
+  ],
   templateUrl: './post.component.html',
   styleUrl: './post.component.css'
 })
